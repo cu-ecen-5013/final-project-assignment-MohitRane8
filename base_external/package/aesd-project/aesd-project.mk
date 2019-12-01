8a7d335e7d7bc1c01d68ab2e54a191a69112f6c4
MohitRane8 <mohit.rane@colorado.edu>
2019-12-01 05:30:54 -0700

    basic uart loopback with fork

--- base_external/package/aesd-project/aesd-project.mk
+++ base_external/package/aesd-project/aesd-project.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_PROJECT_VERSION = 5721c407173eb1918e034411bfdeed57f4660c69 
+AESD_PROJECT_VERSION = 92d0eb122a65f1ad8c4160a11373ade78d1b6a3a 
 # AESD_PROJECT_SITE = git@github.com:cu-ecen-5013/Mohit-Amogh-Final-ProjectSource.git
 AESD_PROJECT_SITE = https://github.com/cu-ecen-5013/Mohit-Amogh-Final-ProjectSource
 AESD_PROJECT_SITE_METHOD = git
