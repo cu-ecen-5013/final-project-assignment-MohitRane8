341856119726b2453d1be58af8de3a03b7c3b843
MohitRane8 <mohit.rane@colorado.edu>
2019-12-03 02:32:22 -0700

    working lux sensor code - reads id register

--- base_external/package/aesd-project/aesd-project.mk
+++ base_external/package/aesd-project/aesd-project.mk
@@ -5,12 +5,12 @@
 #
 ##############################################################
 
-AESD_PROJECT_VERSION = 92d0eb122a65f1ad8c4160a11373ade78d1b6a3a 
+AESD_PROJECT_VERSION = 9a80c136eba62e7fcb4b735133a3e3aa45d51df0 
 # AESD_PROJECT_SITE = git@github.com:cu-ecen-5013/Mohit-Amogh-Final-ProjectSource.git
 AESD_PROJECT_SITE = https://github.com/cu-ecen-5013/Mohit-Amogh-Final-ProjectSource
 AESD_PROJECT_SITE_METHOD = git
 
-# AESD_PROJECT_OVERRIDE_SRCDIR = /home/mohit/Documents/Mohit-Amogh-Final-ProjectSource
+# AESD_PROJECT_OVERRIDE_SRCDIR = /home/mohit/Documents/Mohit-Amogh-Final-ProjectSource/
 
 define AESD_PROJECT_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
@@ -18,7 +18,7 @@ endef
 
 define AESD_PROJECT_INSTALL_TARGET_CMDS
 	# $(INSTALL) -m 0755 $(@D)/uls_sensor $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/test_src/basic_uart/basic_uart $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/test_src/lux_sensor/lux_sensor $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
