c134a183a3a474281a56de747c9f0a8004ad7e2e
MohitRane8 <mohit.rane@colorado.edu>
2019-12-05 20:00:35 -0700

    updated hash

--- base_external/package/aesd-project/aesd-project.mk
+++ base_external/package/aesd-project/aesd-project.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_PROJECT_VERSION = c2332b38a6664d9f6403236f936ab8916c91dbcb 
+AESD_PROJECT_VERSION = 6d2958faa22e30e51520c251a1c076201a626e63 
 # AESD_PROJECT_SITE = git@github.com:cu-ecen-5013/Mohit-Amogh-Final-ProjectSource.git
 AESD_PROJECT_SITE = https://github.com/cu-ecen-5013/Mohit-Amogh-Final-ProjectSource
 AESD_PROJECT_SITE_METHOD = git
@@ -16,7 +16,7 @@ endef
 
 define AESD_PROJECT_INSTALL_TARGET_CMDS
 	# $(INSTALL) -m 0755 $(@D)/uls_sensor $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/test_src/lux_sensor/lux_sensor $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/led_blink $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
